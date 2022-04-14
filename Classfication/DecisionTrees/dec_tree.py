2a06437df413976f475cf404662903a2a21c1915
Ubeydullah Onder <ubeyd03@outloo.com>
2022-04-15 00:22:44 +0300

    Decision tree is loaded

---
+++ Classfication/DecisionTrees/dec_tree.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix,accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+
+df= pd.read_csv('Social_Network_Ads.csv')
+
+X=df.iloc[:,:-1].values
+y=df.iloc[:,-1].values
+
+
+X_train,X_test,y_train,y_test=train_test_split(X,y)
+
+
+#future scaling
+
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+#creating Decision Tree model
+gnb=DecisionTreeClassifier()
+print("Gaussian NB is training")
+gnb.fit(X_train,y_train)
+# making predictions
+y_pred_gnb=gnb.predict(X_test)
+# avaluating
+print(confusion_matrix(y_test,y_pred_gnb))
+print(accuracy_score(y_test,y_pred_gnb))
+
+
+

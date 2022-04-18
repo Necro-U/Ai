27e5217af787130f2ba1aa5beec6a185b7516201
Ubeydullah Onder <ubeyd03@outloo.com>
2022-04-18 12:52:19 +0300

    Eclat loaded

--- Association/Eclat/eclat.py
+++ Association/Eclat/eclat.py
@@ -14,10 +14,9 @@ results = list(rules)
 def inspect(data):
     left=[tuple(l[2][0][0])[0] for l in data]
     right=[tuple(r[2][0][1])[0] for r in data]
-    support=[s[0] for s in data]
-    conf=[c[2][0][2] for c in data]
-    lift=[l[2][0][3] for l in data]
-    return list(zip(left,right,support,conf,lift))
+    # third=[tuple(t[2][0][2])[0] for t in data]
+    support=[s[1] for s in data]
+    return list(zip(left,right,support))
 
-dataF=pd.DataFrame(inspect(results),columns=["LEFT SİDE","RİGHT SİDE","SUPPORT","CONFİDENCE","LİFT"])
-print(dataF)
+dataF=pd.DataFrame(inspect(results),columns=["Product 1","Product 2","SUPPORT"])
+print(dataF.nlargest(10,'SUPPORT'))

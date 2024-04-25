ef517a1530838bfd5ca447dfbde7ec52c346e21b
Necro-U <ubeyd03@outlook.com>
2024-04-25 18:08:34 +0300

    deneme

---
+++ .github/workflows/command.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# https://github.com/Necro-U/github_actions.git
+
+mkdir temp && cd temp
+git init
+remote_repo=https://github.com/Necro-U/github_actions.git
+git config http.sslVerify false
+git config --global user.name "Automated Binder"
+git config --global user.email "actions@users.noreply.github.com"
+
+echo initialized
+git remote add origin "${remote_repo}"
+git show-ref
+git branch --verbose
+
+git lfs install
+
+touch selam.txt
+echo selam >> selam.txt
+
+git add -A 
+git commit -m "Automatik Binder push" || exit 0
+git push -f "https://Necro-U:${GITHUB_TOKEN}@github.com/Necro-U/Tensorflow.git"

c598818e5d630ea21207ec03fdba4a5589cee81c
Necro-U <ubeyd03@outlook.com>
2024-04-25 20:30:52 +0300

    deneme

--- command.sh
+++ command.sh
@@ -22,4 +22,4 @@ git lfs install
 git add .
 git commit -m "Automatik Binder push 2" && echo commited || exit 0
 git show-ref
-git push -f "https://Necro-U:${GITHUB_TOKEN}@github.com/Necro-U/Tensorflow.git" master
+git push -f "https://Necro-U:${GITHUB_TOKEN}@github.com/Necro-U/Tensorflow.git" origin master

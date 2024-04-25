d8945af9fe3a1a991547ee2be713dd4df4f5ce64
Necro-U <ubeyd03@outlook.com>
2024-04-25 19:39:17 +0300

    deneme

--- command.sh
+++ command.sh
@@ -3,15 +3,15 @@
 
 mkdir temp && cd temp
 git init
-remote_repo=https://github.com/Necro-U/github_actions.git
+remote_repo=https://Necro-U:${GITHUB_TOKEN}@github.com/Necro-U/Ai.git
 git config http.sslVerify false
 git config --global user.name "Automated Binder"
 git config --global user.email "actions@users.noreply.github.com"
 
 echo initialized
-# git remote add origin "${remote_repo}"
-# git show-ref
-# git branch --verbose
+git remote add origin "${remote_repo}"
+git show-ref
+git branch --verbose
 
 git lfs install
 

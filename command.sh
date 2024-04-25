dbf125077da263374dfa81e1c4591f133b54c62a
Necro-U <ubeyd03@outlook.com>
2024-04-25 18:19:12 +0300

    deneme

--- command.sh
+++ command.sh
@@ -9,15 +9,15 @@ git config --global user.name "Automated Binder"
 git config --global user.email "actions@users.noreply.github.com"
 
 echo initialized
-git remote add origin "${remote_repo}"
-git show-ref
-git branch --verbose
+# git remote add origin "${remote_repo}"
+# git show-ref
+# git branch --verbose
 
 git lfs install
 
 touch selam.txt
 echo selam >> selam.txt
-
+ls 
 git add -A 
 git commit -m "Automatik Binder push" || exit 0
 git push -f "https://Necro-U:${GITHUB_TOKEN}@github.com/Necro-U/Tensorflow.git"
